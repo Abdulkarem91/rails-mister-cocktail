716c0aea26e1423311e70bf3c48c700d6346d24d
abdulkarem mohammed <abdulkarem.m92@gmail.com>
2020-11-04 15:15:05 +0300

    second challege

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+    belongs_to :ingredient
+    belongs_to :cocktail
+  ​
+    validates :description, presence: true
+    validates :cocktail, uniqueness: { scope: :ingredient }
+  end
---
+++ app/models/ingredient.rb
@@ -0,0 +1,5 @@
+class Ingredient < ApplicationRecord
+    has_many :doses
+  ​
+    validates :name, presence: true, uniqueness: true
+  end
